17c9e80b7ed7a99379b23baad8a2d37d3eb4f90d
Augusto-Peter <85990327+Augusto-Peter@users.noreply.github.com>
2021-10-25 09:41:05 +0200

    commit
    commit augusto

--- src/pages/AboutUs.vue
+++ src/pages/AboutUs.vue
@@ -1,5 +1,52 @@
 <template>
-  <div>About Us</div>
+  <div>
+    <h1 id="title">À PROPOS DE NOUS</h1>
+    <br><br>
+    <h1 class="sous-title">Notre philosophie</h1>
+    <br>
+    <article class="articles">
+      Depuis sa création en 2000, Davide's et Dawit's est une entreprise familiale indépendante engagée envers ses propriétaires, ses collaborateurs, ses clients, ses fournisseurs, l’environnement et la société.
+    </article>
+    <br>
+    <h1 class="sous-title">Histoire</h1>
+    <br>
+    <article class="articles">
+      Les fondateurs inscrivirent la mention « Toujours mieux » sur leurs premiers produits. Tel un fil rouge, ces deux mots suivent l’histoire de plus d’un siècle de Davide's et Dawit's.
+    </article>
+    <br>
+    <h1 class="sous-title">Évolution des affaires</h1>
+    <br>
+    <article class="articles">
+      In the 2020 financial year the company achieved sales of € 4.5 bn. This corresponds to an increase of
+€ 276,6 m, or 6.5%. In Germany, turnover was € 1.33 bn.
+    </article>
+    <br>
+    <h1 class="sous-title">Management</h1>
+    <br>
+    <article class="articles">
+      Davide's et Dawit's. D&amp;D est gérée par quatre directeurs égaux en droits : Dr Davide, Dr Dawit, Dr Rodjack et Mr. Augusto.
+    </article>
+    <br>
+    <h1 class="sous-title">Développement durable</h1>
+    <br>
+    <article class="articles">
+      Pour Davide's et Dawit's, agir durablement n’est pas seulement en lien avec les valeurs et les traditions de l’entreprise, mais constitue aussi la base fondamentale du succès de l’entreprise à long terme.
+    </article>
+    <br>
+    <h1 class="sous-title">Distinctions &amp; tests remportés par Davide's et Dawit's</h1>
+    <br>
+    <article class="articles">Le développement et l’amélioration continus sont à la base de toutes les activités chez Davide's et Dawit's. Les nombreux prix et récompenses obtenus par Miele confirment la réussite de sa stratégie.</article>
+    <br>
+    <h1 class="sous-title">Sites</h1>
+    <br>
+    <article class="articles">Davide's et Dawit's est présent dans 5 pays/régions du monde via ses propres sociétés de distribution et dans quelque 5 autres par le biais d’importateurs.</article>
+    <br>
+    <h1 class="sous-title">Interlocuteurs</h1>
+    <br>
+    <article class="articles">En cas de questions et de demandes, nos interlocuteurs se feront un plaisir de vous aider.</article>
+    <br>
+  </div>
+
 </template>
 
 <script>
@@ -9,5 +56,21 @@ export default {
 </script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap');
+
+#title{
+  background-color: lightgray;
+  color: white;
+  height: 70px;
+  text-align: center;
+  padding-top: 10px;;
+}
+.sous-title{
+  color: red;
+}
+.articles{
+  font-size: xx-large;
+  font-family: 'Dancing Script', cursive;
+}
 
 </style>
