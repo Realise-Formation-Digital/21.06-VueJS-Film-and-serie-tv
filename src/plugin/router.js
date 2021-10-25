5e22e292ac3dc6e706dad35d33b930a4c8f44f7f
Samidava <85990342+Samidava@users.noreply.github.com>
2021-10-25 09:15:53 +0200

    page contact

--- src/plugin/router.js
+++ src/plugin/router.js
@@ -6,9 +6,11 @@ Vue.use(VueRouter)
 import Home from "../pages/Home";
 import Contact from "../pages/Contact";
 
+
 const routes = [
     { path: '/', component: Home },
     { path: '/contact', component: Contact }
+
 ]
 
 const router = new VueRouter({
